bd25514bb948c0bb1e78bfffaf164ea39bd81e21
FelipeBSilva <felipe.silva@grupomultiplica.com.br>
2024-10-10 12:03:45 -0300

    adicionado requirements.txt para facilitar instalacao da lib

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ import cv2
 
 webcam = cv2.VideoCapture(0)
 
-if (webcam.isOpened() == False):  
-    print("Error reading video file") 
-frame_width = int(webcam.get(3)) 
-frame_height = int(webcam.get(4)) 
-   
-size = (frame_width, frame_height) 
-result = cv2.VideoWriter('teste1.avi',
-cv2.VideoWriter_fourcc(*'MJPG'), 10, size)
+if webcam.isOpened() == False:
+    print("Error reading video file")
+frame_width = int(webcam.get(3))
+frame_height = int(webcam.get(4))
+
+size = (frame_width, frame_height)
+result = cv2.VideoWriter("teste1.avi", cv2.VideoWriter_fourcc(*"MJPG"), 10, size)
 
 while True:
     ret, frame = webcam.read()
 
-    cv2.imshow('Frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 webcam.release()
